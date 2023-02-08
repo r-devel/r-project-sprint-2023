0c02fd9a19cac19b78ca5682fbc7e4e8c393118b
Heather Turner <ht@heatherturner.net>
2023-02-08 17:15:47 +0000

    set up event website

---
+++ R/functions.R
@@ -0,0 +1,25 @@
+library(htmltools)
+person_info <- function(name = "A Person",
+                        affiliation = "Role\nInstitution",
+                        pic = "media/reshot-icon-human-person-6P2MCTEQ95.svg",
+                        alt = "Generic person icon",
+                        website = NULL,
+                        email = NULL){
+  res <- list(img(src = pic, alt = alt, style = "width:55%"),
+              paste('<br/><b>', name, '</b>'),
+              paste('<br/>', affiliation, '<br/>'))
+  if (!is.null(website)){
+    link <- a(img(src = "media/reshot-icon-link-TPKSVGQNXF.svg", height = "30",
+                  style = "margin:2px; vertical-align:middle;", border = "0"),
+              href = website, target = "_blank")
+    res <- c(res, list(link))
+    
+  }
+  if (!is.null(email)){
+    link <- a(img(src = "media/reshot-icon-email-P9WA8LS724.svg", 
+                  height = "30", style = "margin:2px; vertical-align:middle;", border = "0"),
+              href = paste0("mailto:", email))
+    res <- c(res, list(link))
+  }
+  cat(vapply(res, as.character, character(1)), sep = "\n")
+}
