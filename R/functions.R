a5b32c8710eea654d55d1bc0eb0fef7034063934
Heather Turner <ht@heatherturner.net>
2023-03-27 15:51:58 +0100

    add participants page

--- R/functions.R
+++ R/functions.R
@@ -1,4 +1,6 @@
 library(htmltools)
+
+# Used for local committee
 person_info <- function(name = "A Person",
                         affiliation = "Role\nInstitution",
                         pic = "media/reshot-icon-human-person-6P2MCTEQ95.svg",
@@ -20,6 +22,39 @@ person_info <- function(name = "A Person",
                   height = "30", style = "margin:2px; vertical-align:middle;", border = "0"),
               href = paste0("mailto:", email))
     res <- c(res, list(link))
+  }
+  cat(vapply(res, as.character, character(1)), sep = "\n")
+}
+
+# Used for participant list
+participant_info <- function(name = "A Person",
+                             affiliation = NULL,
+                             pic = "/media/reshot-icon-human-person-6P2MCTEQ95.svg",
+                             alt = "Generic person icon",
+                             link = NULL, # main website
+                             github = NULL,
+                             mastodon = NULL,
+                             twitter = NULL,
+                             linkedin = NULL){
+  res <- list(img(src = pic, alt = alt, style = "width:55%"))
+  if (!is.null(link)){
+    res <- c(res, 
+             list(paste('<br/>', a(name, href = website, target = "_blank", 
+                                   style = "text-decoration:none"), '</b>')))
+  } else {
+    res <- c(res, list(paste('<br/><b>', name, '</b>')))
+  }
+  if (!is.null(affiliation)) {
+    res <- c(res, list(paste('<br/>', affiliation, '<br/>')))
+  }
+  links <- c("link", "linkedin", "mastodon", "twitter", "github")
+  for (x in links) {
+    if (!is.null(get(x))){
+      hyperlink <- a(paste('{{< bi', x, ' size=5em color=#2ecaf9 >}}'),
+                     href = get(x), target = "_blank", 
+                     style = "text-decoration:none")
+      res <- c(res, list(hyperlink))
+    }
   }
   cat(vapply(res, as.character, character(1)), sep = "\n")
 }
